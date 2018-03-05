c38bdaf395c91e1e1a8c1967cf86cbc8f87481c5
Arye Dov Eidelman <aryedov.e@gmail.com>
2018-03-05 18:35:01 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,5 +4,10 @@
 def key_for_min_value(name_hash)
   result = ""
   lowest_value 100000
-  name_hash.each{ |key, value| result = key if }
+  name_hash.each do |key, value|
+    if value < lowest_value
+      result = key
+      lowest_value = value
+    end
+  end
 end
